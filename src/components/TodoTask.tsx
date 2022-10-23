2bd19140367ec84399714dcaa1381743c877496c
serhiy hvala <serhiyhvaladew@gmail.com>
2022-10-23 19:43:26 +0300

    Create Functionality to add and delete Tasks

---
+++ src/components/TodoTask.tsx
@@ -0,0 +1,19 @@
+import { ITask } from "../interfaces"
+
+interface Props {
+    task: ITask
+    completeTask(taskNameToDelete: string):void
+}
+
+export const TodoTask = ({task, completeTask}:Props) => {
+  return (
+    <div className="task">
+        <div className="content">
+            <span>{task.taskName}</span>
+            <span>{task.deadLine}</span>
+        </div>
+        <button onClick={() => completeTask(task.taskName)}>X</button>
+    </div>
+  )
+}
+
